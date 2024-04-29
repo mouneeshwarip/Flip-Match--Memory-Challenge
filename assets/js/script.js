777ce0ee7b2b89d0513b349cf7e920b9f8922a11
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-04-29 14:56:48 +0000

    add functions to the controls

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,31 @@ const playagainbtn=document.getElementById('playagainbtn');
 const cardcontainer=document.querySelector('.card-container');
 const cards=document.querySelectorAll('.card');
 const endgamebtn=document.getElementById('endgamebtn');
+let score=0;
+let timer=0;
+let isvolumeon=false; //default volume is set to off
+
+//Functions to update score, timer and to turn the vol on
+function updatescore{
+      
+}
+
+function updatetimer{
+
+}
+function togglevol{
+    isvolumeon=!isvolumeon;
+    const volumeobutton=document.getElementById('vol-btn');
+    volumeobutton.innerText=isvolumeon ? 'Turn-Off' : 'Turn-On';
+}
+
+function formatTime(seconds) {
+    // Format the time as mm:ss
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
+
 
 //Function to start the game
 function startgame{
@@ -25,12 +50,13 @@ function endgame{
 
 }
 
-//Event listener for startgame,endgame, playagain and cardflips
+//Event listener for startgame,endgame, playagain, cardflips and toggle vol
 startgamebtn.addEventlistener('click',startgame);
 endgamebtn.addEventListener('click',endgame);
 cards.forEach(function(card){
     card.addEventListener('click',handlecardclick);
 })
 playagainbtn.addEventListener('click',playagain);
+document.getElementById('vol-btn').addEventListener('click',togglevol);
 
 
